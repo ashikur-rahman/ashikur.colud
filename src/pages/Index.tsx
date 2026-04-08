ff939174cd7ed654de6dc1210ae6dea919985700
ashikur-rahman <shuvocste@gmail.com>
2026-04-08 13:58:12 +0100

    VelocityOS chatbot 2

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -28,9 +28,6 @@ const Index = () => (
   </div>
 );
 
-<script src="https://app.velocityos.app/chatbot/widget.js"
-  data-key="vos_cb_tpDpz...[regenerate to see full key]"
-  data-position="bottom-right"
-  data-theme="light"></script>
+
 
 export default Index;
